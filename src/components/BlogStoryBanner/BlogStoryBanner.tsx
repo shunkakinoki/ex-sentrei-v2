a44f3ec8682c743079a025ed974cf97c26e9ea27
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-19 19:13:02 +0900

    feat: 🎸 ini clsx w/ blog story group

--- src/components/BlogStoryBanner/BlogStoryBanner.tsx
+++ src/components/BlogStoryBanner/BlogStoryBanner.tsx
@@ -3,5 +3,9 @@ export interface Props {
 }
 
 export default function BlogStoryBanner({title}: Props): JSX.Element {
-  return <div className="group-hover:text-purple-300">{title}</div>;
+  return (
+    <div className="mt-5 group-hover:text-purple-300">
+      <h3 className="text-3xl text-center md:text-left md:pl-8">{title}</h3>
+    </div>
+  );
 }
--- src/components/BlogStoryPreview/BlogStoryPreview.tsx
+++ src/components/BlogStoryPreview/BlogStoryPreview.tsx
@@ -1,9 +1,20 @@
 import BlogStoryImage from "@/components/BlogStoryImage";
 import BlogStoryBanner from "@/components/BlogStoryBanner";
+import clsx from "clsx";
 
-export default function BlogStoryPreview(): JSX.Element {
+export interface Props {
+  variant?: "small" | "middle" | "large";
+}
+
+export default function BlogStoryPreview({
+  variant = "small",
+}: Props): JSX.Element {
   return (
-    <div className="group">
+    <div
+      className={clsx("group", {
+        "grid-cols-1": variant === "small",
+      })}
+    >
       <BlogStoryImage />
       <BlogStoryBanner title="First Post" />
     </div>
