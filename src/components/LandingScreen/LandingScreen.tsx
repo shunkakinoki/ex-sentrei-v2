6c50b874157e853cbf32939e31c4ce6c23dd3236
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-19 13:46:42 +0900

    feat: 🎸 ini landing feature

--- src/components/LandingScreen/LandingScreen.tsx
+++ src/components/LandingScreen/LandingScreen.tsx
@@ -1,11 +1,13 @@
 import LandingBanner from "@/components/LandingBanner";
 import LandingHeader from "@/components/LandingHeader";
+import LandingFeature from "@/components/LandingFeature";
 
 export default function LandingScreen(): JSX.Element {
   return (
     <div className="container min-h-screen px-5 mx-auto">
       <LandingHeader />
       <LandingBanner />
+      <LandingFeature />
     </div>
   );
 }
