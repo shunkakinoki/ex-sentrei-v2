92112cc77debb0e9ad95a842aed88e8cdd0b6344
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-19 18:02:41 +0900

    feat: 🎸 ini blog banner

--- src/components/DemoScreen/DemoScreen.tsx
+++ src/components/DemoScreen/DemoScreen.tsx
@@ -1,10 +1,15 @@
 import LandingHeader from "@/components/LandingHeader";
+import BlogBanner from "@/components/BlogBanner";
 import RootContainer from "@/components/RootContainer";
 
 export default function DemoScreen(): JSX.Element {
   return (
     <RootContainer>
       <LandingHeader />
+      <BlogBanner
+        title="My awesome newsletter."
+        subtitle="Relax. I'm so awesome"
+      />
     </RootContainer>
   );
 }
