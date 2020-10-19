ce5d73455c9d0aac2c55822dcc85506a49c79011
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-20 07:57:23 +0900

    perf: ⚡️ refactor transition for blog story image

--- src/components/BlogStoryImage/BlogStoryImage.tsx
+++ src/components/BlogStoryImage/BlogStoryImage.tsx
@@ -5,7 +5,7 @@ export interface Props {
 
 export default function BlogStoryImage({src, title}: Props): JSX.Element {
   return (
-    <div className="transition-shadow duration-300 ease-in-out shadow-none md:shadow-lg xl:shadow-xl group-hover:shadow-none md:group-hover:shadow-2xl">
+    <div className="transition duration-300 ease-in-out transform shadow-none md:hover:-translate-y-1 md:shadow-lg xl:shadow-xl group-hover:shadow-none md:group-hover:shadow-2xl">
       <img
         alt={`Story Cover for ${title}`}
         src={src}
