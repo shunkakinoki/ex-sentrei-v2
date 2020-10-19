f6b5ad4f4335de353e46744712a7da28c58bc3b2
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-19 22:23:06 +0900

    feat: 🎸 ini blog story preview component

--- src/components/BlogStoryBanner/BlogStoryBanner.tsx
+++ src/components/BlogStoryBanner/BlogStoryBanner.tsx
@@ -6,7 +6,7 @@ export interface Props {
 export default function BlogStoryBanner({title, subtitle}: Props): JSX.Element {
   return (
     <div className="mt-5">
-      <div className="transition duration-500 ease-in-out group-hover:text-purple-300 ">
+      <div className="transition duration-500 ease-in-out group-hover:text-pink-500">
         <h3 className="text-3xl text-center md:text-left md:pl-8">{title}</h3>
       </div>
       <h6 className="overflow-hidden text-gray-700 truncate-2-lines md:text-left md:pl-8">
