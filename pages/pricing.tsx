5ce7582a2d276138d7ee292dc2cad27a48473508
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-19 16:30:58 +0900

    feat: 🎸 ini pricing screen

---
+++ pages/pricing.tsx
@@ -0,0 +1,8 @@
+import {NextPage} from "next";
+import PricingScreen from "@/components/PricingScreen";
+
+const Pricing: NextPage = () => {
+  return <PricingScreen />;
+};
+
+export default Pricing;
---
+++ src/components/PricingScreen/PricingScreen.tsx
@@ -0,0 +1,9 @@
+import LandingHeader from "@/components/LandingHeader";
+
+export default function PricingScreen(): JSX.Element {
+  return (
+    <div className="container min-h-screen px-5 mx-auto">
+      <LandingHeader />
+    </div>
+  );
+}
