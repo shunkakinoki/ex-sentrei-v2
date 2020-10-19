a4cb5638b83ce343b3a739d436c5a021e0525e28
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-19 17:01:06 +0900

    feat: 🎸 ini pricing banner

--- src/components/PricingScreen/PricingScreen.tsx
+++ src/components/PricingScreen/PricingScreen.tsx
@@ -1,9 +1,11 @@
 import LandingHeader from "@/components/LandingHeader";
+import PricingBanner from "@/components/PricingBanner";
 
 export default function PricingScreen(): JSX.Element {
   return (
     <div className="container min-h-screen px-5 mx-auto">
       <LandingHeader />
+      <PricingBanner />
     </div>
   );
 }
