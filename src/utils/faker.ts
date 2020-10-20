ab2369f30155bbf222eea6e1b6933708a3c07d4d
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-20 20:15:43 +0900

    feat: 🎸 ini faker markdown

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -36,3 +36,85 @@ export const createArticles = (num = 6): Article[] => {
 export const createAuthors = (num = 3): Author[] => {
   return new Array(num).fill(undefined).map(createAuthor);
 };
+
+// Inspired by: https://github.com/Marak/faker.js/blob/4bd0935460ab62b72593b42471699d2044c7a53a/lib/markdown.js
+export const createMdHeader = (num = 3): string => {
+  const head = new Array(num).join("#");
+  return [head, faker.lorem.word()].join(" ");
+};
+
+export const createMdEmphasis = (num = 3): string => {
+  const types = ["_", "~", "*", "**"];
+  const sentence = faker.lorem.sentence(num);
+  const type = types[Math.floor(Math.random() * types.length + 1)];
+  return type + sentence + type;
+};
+
+export const createMdTable = (num = 3): string => {
+  const table = [
+    "| header1 | header2 | header3 |",
+    "|:-----:|:-----:|:-----:|",
+  ];
+  for (let i = 0; num > i; i += 1) {
+    const line = ["|", faker.lorem.words(num).split(" ").join("|"), "|"].join(
+      "",
+    );
+    table.push(line);
+  }
+  return table.join("\n");
+};
+
+export const createMdList = (num = 3): string => {
+  const sentences = new Array(num).fill(undefined).map(() => {
+    return faker.lorem.sentence();
+  });
+  let list: string[] | undefined;
+  if (Math.random() > 0.5) {
+    sentences.forEach(sentence => {
+      list?.push(`* ${sentence}`);
+    });
+  } else {
+    sentences.forEach((sentence, index) => {
+      list?.push([index + 1, ". ", sentence].join(""));
+    });
+  }
+  return list?.join("\n") || "";
+};
+
+export const createMdBlock = (num = 3): string => {
+  if (Math.random() > 0.5) {
+    return ["`", faker.lorem.lines(num), "`"].join();
+  }
+  return ["```javascript", faker.lorem.lines(num), "```"].join();
+};
+
+export const createMarkdown = (num = 3): string => {
+  let blocks: string[] | undefined;
+  for (let i = 0; num > i; i += 1) {
+    const flag = Math.floor(Math.random() * 6);
+    switch (flag) {
+      case 0:
+        blocks?.push(faker.lorem.paragraphs(Math.floor(Math.random() * 3)));
+        break;
+      case 1:
+        blocks?.push(createMdEmphasis(Math.floor(Math.random() * 3)));
+        break;
+      case 2:
+        blocks?.push(createMdHeader(Math.floor(Math.random() * 3)));
+        break;
+      case 3:
+        blocks?.push(createMdTable(Math.floor(Math.random() * 3)));
+        break;
+      case 4:
+        blocks?.push(createMdList(Math.floor(Math.random() * 3)));
+        break;
+      case 5:
+        blocks?.push(createMdBlock(Math.floor(Math.random() * 3)));
+        break;
+      default: {
+        blocks?.push(faker.lorem.paragraph());
+      }
+    }
+  }
+  return blocks?.join("\n") || faker.lorem.paragraph();
+};
