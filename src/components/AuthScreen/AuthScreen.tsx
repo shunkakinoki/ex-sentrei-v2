de6ec7dffe2490fe08e6527d9e3d316f77d71582
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-21 22:23:44 +0900

    perf: ⚡️ ini auth screen

--- src/components/AuthScreen/AuthScreen.tsx
+++ src/components/AuthScreen/AuthScreen.tsx
@@ -1,14 +1,13 @@
 import ContainerCenter from "@/components/ContainerCenter";
-import SubscribeForm from "@/components/SubscribeForm";
 import ContainerRoot from "@/components/ContainerRoot";
 import LandingHeader from "@/components/LandingHeader";
 
-export default function SubscribeScreen(): JSX.Element {
+export default function AuthScreen(): JSX.Element {
   return (
     <ContainerRoot>
       <LandingHeader />
       <ContainerCenter>
-        <SubscribeForm />
+        <></>
       </ContainerCenter>
     </ContainerRoot>
   );
