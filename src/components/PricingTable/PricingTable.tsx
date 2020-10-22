a0f8e2873249dfa470806144c752a38fa6b408af
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-23 05:30:56 +0900

    feat: 🎸 ini pricing table

---
+++ src/components/PricingTable/PricingTable.tsx
@@ -0,0 +1,13 @@
+import PricingTableCard from "@/components/PricingTableCard";
+
+export default function PricingTable(): JSX.Element {
+  return (
+    <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+      <div className="relative space-y-8 sm:space-y-10 md:space-y-12 lg:space-y-0 lg:gap-x-3 lg:grid lg:grid-cols-10">
+        <PricingTableCard plan="free" />
+        <PricingTableCard plan="pro" />
+        <PricingTableCard plan="premium" />
+      </div>
+    </div>
+  );
+}
