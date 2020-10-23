7bada1edcaa7084de98c00c456961d1010b4e85a
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-23 18:26:13 +0900

    perf: ⚡️ refactor header demo

--- src/components/HeaderDemoMenu/HeaderDemoMenu.tsx
+++ src/components/HeaderDemoMenu/HeaderDemoMenu.tsx
@@ -28,24 +28,24 @@ export default function HeaderDemoMenu({open}: Props): JSX.Element {
                   <a className="flex items-start p-3 -m-3 space-x-4 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
                     <svg
                       className="flex-shrink-0 w-6 h-6 text-pink-600"
-                      xmlns="http://www.w3.org/2000/svg"
                       fill="none"
-                      viewBox="0 0 24 24"
                       stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                        strokeWidth={2}
+                        d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
                       />
                     </svg>
                     <div className="space-y-1">
                       <p className="text-base font-medium leading-6 text-gray-900">
-                        Analytics
+                        Built-in newsletter
                       </p>
                       <p className="text-sm leading-5 text-gray-500">
-                        Get a better understanding of your audience.
+                        Send email newsletters to engage with your customers
                       </p>
                     </div>
                   </a>
@@ -71,7 +71,88 @@ export default function HeaderDemoMenu({open}: Props): JSX.Element {
                         Custom editor
                       </p>
                       <p className="text-sm leading-5 text-gray-500">
-                        Write your content in markdown with our dedicated editor
+                        Write your content in markdown with our dedicated
+                        markdown editor
+                      </p>
+                    </div>
+                  </a>
+                </Link>
+                <Link href="/demo">
+                  <a className="flex items-start p-3 -m-3 space-x-4 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
+                    <svg
+                      className="flex-shrink-0 w-6 h-6 text-pink-600"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    <div className="space-y-1">
+                      <p className="text-base font-medium leading-6 text-gray-900">
+                        Custom domains
+                      </p>
+                      <p className="text-sm leading-5 text-gray-500">
+                        Brand your content on top of the blazing edge performant
+                        web network.
+                      </p>
+                    </div>
+                  </a>
+                </Link>
+                <Link href="/demo/dashboard">
+                  <a className="flex items-start p-3 -m-3 space-x-4 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
+                    <svg
+                      className="w-6 h-6 text-pink-600"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                      />
+                    </svg>
+                    <div className="space-y-1">
+                      <p className="text-base font-medium leading-6 text-gray-900">
+                        Custom integrations
+                      </p>
+                      <p className="text-sm leading-5 text-gray-500">
+                        Integrate with 3rd party services to further delight
+                        your audience.
+                      </p>
+                    </div>
+                  </a>
+                </Link>
+                <Link href="/demo/dashboard">
+                  <a className="flex items-start p-3 -m-3 space-x-4 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
+                    <svg
+                      className="flex-shrink-0 w-6 h-6 text-pink-600"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                      />
+                    </svg>
+                    <div className="space-y-1">
+                      <p className="text-base font-medium leading-6 text-gray-900">
+                        Dedicated analytics
+                      </p>
+                      <p className="text-sm leading-5 text-gray-500">
+                        Get a better understanding of your audience.
                       </p>
                     </div>
                   </a>
@@ -99,58 +180,6 @@ export default function HeaderDemoMenu({open}: Props): JSX.Element {
                       <p className="text-sm leading-5 text-gray-500">
                         Charge your customers for your own content with no
                         margins at all
-                      </p>
-                    </div>
-                  </a>
-                </Link>
-                <Link href="/demo/dashboard">
-                  <a className="flex items-start p-3 -m-3 space-x-4 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
-                    <svg
-                      className="flex-shrink-0 w-6 h-6 text-pink-600"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
-                      />
-                    </svg>
-                    <div className="space-y-1">
-                      <p className="text-base font-medium leading-6 text-gray-900">
-                        Newsletter
-                      </p>
-                      <p className="text-sm leading-5 text-gray-500">
-                        Send email newsletters to engage with your customers
-                      </p>
-                    </div>
-                  </a>
-                </Link>
-                <Link href="/demo">
-                  <a className="flex items-start p-3 -m-3 space-x-4 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50">
-                    <svg
-                      className="flex-shrink-0 w-6 h-6 text-pink-600"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                    <div className="space-y-1">
-                      <p className="text-base font-medium leading-6 text-gray-900">
-                        Wholesome platform
-                      </p>
-                      <p className="text-sm leading-5 text-gray-500">
-                        Say goodbye to annoying ads and platform subscriptions
                       </p>
                     </div>
                   </a>
