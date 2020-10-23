55d1f89e3c8b17f2d2174f2b12fbc90e7c2574a2
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-23 21:32:07 +0900

    perf: ⚡️ ini settings page

---
+++ src/components/SettingsScreen/SettingsScreen.tsx
@@ -0,0 +1,10 @@
+import HeaderRoot from "@/components/HeaderRoot";
+import ContainerRoot from "@/components/ContainerRoot";
+
+export default function SettingsScreen(): JSX.Element {
+  return (
+    <ContainerRoot>
+      <HeaderRoot />
+    </ContainerRoot>
+  );
+}
