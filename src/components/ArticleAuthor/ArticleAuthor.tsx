19e9fc3ac2648d645cd46a2d974858d8b19f0482
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-24 20:26:45 +0900

    feat: 🎸 ini article authors

--- src/components/ArticleAuthor/ArticleAuthor.tsx
+++ src/components/ArticleAuthor/ArticleAuthor.tsx
@@ -1,41 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
-import Article from "@/types/Article";
 import Author from "@/types/Author";
 
-export interface Props extends Pick<Article, "date" | "time"> {
-  authors: Author[];
+export interface Props {
+  author: Author;
 }
 
-export default function ArticleAuthor({
-  authors,
-  date,
-  time,
-}: Props): JSX.Element {
+export default function ArticleAuthor({author}: Props): JSX.Element {
   return (
-    <section className="px-4 py-2 mt-2">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center flex-1">
-          <Image
-            className="inline object-cover w-12 h-12 mr-2 rounded-full"
-            unoptimized
-            unsized
-            src={authors[0].image ?? ""}
-            alt={`Author ${authors[0].name}`}
-          />
-          <div className="flex flex-col mx-2">
-            <a className="font-semibold text-gray-700 hover:underline">
-              {authors[0].name}
-            </a>
-            <span className="mx-1 text-xs text-gray-600">
-              {new Date(date).toDateString()}
-            </span>
-          </div>
-        </div>
-        <h3 className="pr-1 mt-1 text-xs text-gray-600 md:pr-2 md:text-md">
-          {time} min read
-        </h3>
-      </div>
-    </section>
+    <Link href={`/demo/profile/${author.namespace}`}>
+      <a>
+        <Image
+          className="inline object-cover w-12 h-12 mr-2 rounded-full"
+          unoptimized
+          unsized
+          src={author.image ?? ""}
+          alt={`Author ${author.name}`}
+        />
+      </a>
+    </Link>
   );
 }
---
+++ src/components/ArticleAuthorsMenu/ArticleAuthorsMenu.tsx
@@ -0,0 +1,3 @@
+export default function ArticleAuthorsMenu(): JSX.Element {
+  return <div />;
+}
