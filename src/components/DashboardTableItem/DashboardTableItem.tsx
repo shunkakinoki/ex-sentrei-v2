5f026c66251d849cc551477ff54b822f09482b97
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-25 21:44:38 +0900

    perf: ⚡️ refactor dashboard table item group hover

--- src/components/DashboardTableItem/DashboardTableItem.tsx
+++ src/components/DashboardTableItem/DashboardTableItem.tsx
@@ -53,32 +53,32 @@ export default function DashboardTableItem({
         </div>
       </div>
       <div className="flex mt-5 md:mt-0 md:ml-4">
-        <span className="rounded-md shadow-sm ">
+        <span className="rounded-md shadow-sm group">
           <button
             type="button"
-            className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md group-hover:text-pink-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50"
           >
             <svg
-              className="w-5 h-5 mr-2 -ml-1 text-gray-500"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
+              className="w-5 h-5 mr-2 -ml-1 group-hover:text-pink-500"
               fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
             </svg>
             Edit
           </button>
         </span>
-        <span className="ml-3 rounded-md shadow-sm ">
+        <span className="ml-3 rounded-md shadow-sm group">
           <button
             type="button"
-            className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50"
+            className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md group-hover:text-pink-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50"
           >
             <svg
-              className="w-5 h-5 mr-2 -ml-1 text-gray-500"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
+              className="w-5 h-5 mr-2 -ml-1 group-hover:text-pink-500"
               fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
                 fillRule="evenodd"
