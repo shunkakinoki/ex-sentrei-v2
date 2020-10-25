80b29a0c838434e020fe930e763c50065c0969ef
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-25 22:17:40 +0900

    perf: ⚡️ refactor settings screen

--- src/components/SettingsScreen/SettingsScreen.tsx
+++ src/components/SettingsScreen/SettingsScreen.tsx
@@ -7,9 +7,9 @@ export default function SettingsScreen(): JSX.Element {
   return (
     <ContainerRoot>
       <HeaderRoot />
-      <div className="container mx-auto my-6 md:mt-10 ">
+      <div className="container mx-3 my-6 md:mx-6 md:mt-10">
         <div className="flex items-center mb-6 md:mb-8 lg:mb-12">
-          <h3 className="flex-grow-0 px-2 py-2 m-2 text-3xl font-semibold text-gray-700 md:text-4xl">
+          <h3 className="flex-grow-0 px-2 py-2 text-3xl font-semibold text-gray-700 md:text-4xl">
             Settings
           </h3>
         </div>
