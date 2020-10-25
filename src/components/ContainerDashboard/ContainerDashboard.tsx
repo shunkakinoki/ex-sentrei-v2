93d77d69cad3f41df782b377f21375e9e6c33d94
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-25 20:34:23 +0900

    perf: ⚡️ refactor container dashboard

--- src/components/ContainerDashboard/ContainerDashboard.tsx
+++ src/components/ContainerDashboard/ContainerDashboard.tsx
@@ -15,7 +15,7 @@ export default function ContainerDashboard({
 }: Props): JSX.Element {
   return (
     <div className="flex flex-col justify-center lg:flex-row ">
-      <div className="w-full mt-8 lg:w-1/6 sm:mt-12 md:mt-16 ">
+      <div className="w-full mt-2 lg:w-1/6 sm:mt-4 md:mt-8 lg:mt-12 ">
         <div className="flex flex-col ">
           <TabDashboard
             active={type === "articles"}
