3fbfbb61cdf68ae28582d1e35d71e6430a5f5cc6
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-28 00:42:42 +0900

    feat: 🎸 ini firebase config

--- src/components/AppRoot/AppRoot.tsx
+++ src/components/AppRoot/AppRoot.tsx
@@ -1,6 +1,7 @@
 import {useEffect} from "react";
 
 import auth from "@/firebase/auth";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import db from "@/firebase/db";
 import performance from "@/firebase/performance";
 
@@ -9,10 +10,6 @@ export default function AppRoot(): null {
     // eslint-disable-next-line no-console
     auth.onAuthStateChanged(console.log);
   }, []);
-
-  useEffect(() => {
-    db.settings({});
-  });
 
   useEffect(() => {
     performance();
