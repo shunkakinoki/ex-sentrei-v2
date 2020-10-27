68f3cd118ce49ad4c260dcab999b0389a7b94186
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-27 16:52:00 +0900

    perf: ⚡️ refactor icon hover

--- src/components/EditorHeader/EditorHeader.tsx
+++ src/components/EditorHeader/EditorHeader.tsx
@@ -7,31 +7,10 @@ export default function EditorHeader(): JSX.Element {
     <div className="relative z-20">
       <div className="px-4 mx-auto sm:px-6">
         <div className="flex justify-between py-6 border-b-2 border-gray-100 i item-center md:justify-start md:space-x-10">
-          <span className="inline-flex rounded-md shadow-sm">
-            <Link href="/demo/dashboard">
-              <a className="py-2">
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M15 19l-7-7 7-7"
-                  />
-                </svg>
-              </a>
-            </Link>
-          </span>
-          <div className="lg:w-0 lg:flex-1" />
-          <span className="inline-flex rounded-md shadow-sm">
-            <div className="py-2">
+          <Link href="/demo/dashboard">
+            <a className="inline-flex items-center p-2 text-gray-500 rounded-full md:p-3 hover:bg-pink-50">
               <svg
-                className="w-6 h-6 text-gray-600"
+                className="w-6 h-6"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -41,11 +20,28 @@ export default function EditorHeader(): JSX.Element {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                  d="M15 19l-7-7 7-7"
                 />
               </svg>
-            </div>
-          </span>
+            </a>
+          </Link>
+          <div className="lg:w-0 lg:flex-1" />
+          <a className="inline-flex items-center p-2 text-gray-500 rounded-full md:p-3 hover:bg-pink-50">
+            <svg
+              className="w-6 h-6 text-gray-600"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+              />
+            </svg>
+          </a>
           <EditorHeaderSwitch />
           <span className="inline-flex rounded-md shadow-sm">
             <Link href="/demo/dashboard">
