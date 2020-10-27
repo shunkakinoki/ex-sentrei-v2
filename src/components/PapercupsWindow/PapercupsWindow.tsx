94fe87b983cafbd403ee02ed871201ab50118134
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-27 11:31:35 +0900

    feat: 🎸 ini sales support

---
+++ src/components/PapercupsWindow/PapercupsWindow.tsx
@@ -0,0 +1,21 @@
+import {ChatWindow} from "@papercups-io/chat-widget";
+
+export interface Props {
+  isSales?: boolean;
+}
+
+export default function PapercupsWindow({isSales = false}: Props): JSX.Element {
+  return (
+    <div className="h-32 shadow-lg md:h-96 lg:h-128 xl:h-192 9 m-9 md:m-8 lg:m-10 xl:m-12">
+      <ChatWindow
+        accountId={process.env.NEXT_PUBLIC_PAPERCUPS_ID ?? ""}
+        title="Welcome to Sentrei Support"
+        subtitle="Ask us anything in the chat window below 😊"
+        newMessagePlaceholder="Start typing ..."
+        primaryColor="#ffa7d7"
+        greeting="Hi there! How can we help you?"
+        requireEmailUpfront={isSales}
+      />
+    </div>
+  );
+}
--- src/components/SupportScreen/SupportScreen.tsx
+++ src/components/SupportScreen/SupportScreen.tsx
@@ -1,10 +1,17 @@
+import dynamic from "next/dynamic";
+
 import ContainerRoot from "@/components/ContainerRoot";
 import HeaderRoot from "@/components/HeaderRoot";
+
+const PapercupsWindow = dynamic(() => import("@/components/PapercupsWindow"), {
+  ssr: false,
+});
 
 export default function SupportScreen(): JSX.Element {
   return (
     <ContainerRoot>
       <HeaderRoot />
+      <PapercupsWindow />
     </ContainerRoot>
   );
 }
