83226dd15277812f0ab2773a78696709e3a658cd
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-27 16:36:14 +0900

    perf: ⚡️ ini editor header menu button icon

--- src/components/EditorHeader/EditorHeader.tsx
+++ src/components/EditorHeader/EditorHeader.tsx
@@ -28,9 +28,27 @@ export default function EditorHeader(): JSX.Element {
             </Link>
           </span>
           <div className="lg:w-0 lg:flex-1" />
+          <span className="inline-flex rounded-md shadow-sm">
+            <div className="py-2">
+              <svg
+                className="w-6 h-6 text-gray-600"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                />
+              </svg>
+            </div>
+          </span>
           <EditorHeaderSwitch />
           <span className="inline-flex rounded-md shadow-sm">
-            <Link href="/signup">
+            <Link href="/demo/dashboard">
               <a className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap transition duration-150 ease-in-out bg-pink-400 border border-transparent rounded-md hover:bg-pink-500 focus:outline-none focus:border-pink-800 focus:shadow-outline-pink active:bg-pink-700">
                 Save
               </a>
