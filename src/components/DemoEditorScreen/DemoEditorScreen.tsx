20e28e54352f84613d20648df36f0738ba61077a
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-27 16:55:59 +0900

    perf: ⚡️ ini demo editor screen

---
+++ src/components/DemoEditorScreen/DemoEditorScreen.tsx
@@ -0,0 +1,11 @@
+import DemoBanner from "@/components/DemoBanner";
+import EditorScreen from "@/components/EditorScreen";
+
+export default function DemoEditorScreen(): JSX.Element {
+  return (
+    <>
+      <DemoBanner />
+      <EditorScreen />
+    </>
+  );
+}
---
+++ src/components/DemoEditorScreen/index.ts
@@ -0,0 +1,2 @@
+export {default} from "./DemoEditorScreen";
+export type {Props} from "./DemoEditorScreen";
