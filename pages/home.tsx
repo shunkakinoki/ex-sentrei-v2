d3e9a08908b65f9998cab621e969a414c03f5df3
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-27 09:57:21 +0900

    feat: 🎸 ini redirect landing

---
+++ pages/home.tsx
@@ -0,0 +1,9 @@
+import {NextPage} from "next";
+
+import LandingScreen from "@/components/LandingScreen";
+
+const Home: NextPage = () => {
+  return <LandingScreen />;
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,78 @@
-import {NextPage} from "next";
+import {GetServerSideProps, InferGetServerSidePropsType} from "next";
+import {useEffect} from "react";
 
-import LandingScreen from "@/components/LandingScreen";
+import BlogScreen, {Props as BlogScreenProps} from "@/components/BlogScreen";
+import {totalArticlePages} from "@/const/demo";
+import Article from "@/types/Article";
+import Blog from "@/types/Blog";
+import {createArticles, createBlog} from "@/utils/faker";
 
-const Index: NextPage = () => {
-  return <LandingScreen />;
+export type Props = Omit<BlogScreenProps, "articles" | "blog" | "total"> & {
+  articles: string;
+  blog: string;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  res,
+  req,
+  // eslint-disable-next-line @typescript-eslint/require-await
+}) => {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=1, stale-while-revalidate=59",
+  );
+
+  if (
+    req.headers.host === "localhost:3000" ||
+    req.headers.host === "sentrei.com"
+  ) {
+    return {
+      unstable_redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+
+  const articles = createArticles();
+  const blog = createBlog();
+
+  if (req.headers.host === "demo.sentrei.com") {
+    return {
+      props: {
+        articles: JSON.stringify(articles),
+        blog: JSON.stringify(blog),
+        current: 1,
+        namespace: req.headers.host,
+      },
+    };
+  }
+
+  return {
+    unstable_notFound: true,
+  };
+};
+
+const Index = ({
+  articles,
+  blog,
+  current,
+  namespace,
+}: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element => {
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(`host: ${namespace ?? ""}`);
+  });
+
+  return (
+    <BlogScreen
+      articles={JSON.parse(articles) as Article[]}
+      blog={JSON.parse(blog) as Blog}
+      current={current}
+      total={totalArticlePages}
+      namespace={namespace}
+    />
+  );
 };
 
 export default Index;
