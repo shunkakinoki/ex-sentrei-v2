74cdbef8c535d1eb255d37b353a0e9f08ac79c74
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-30 17:22:06 +0900

    feat: 🎸 migrate to react-toastify from global alert

---
+++ src/components/GlobalToast/GlobalToast.tsx
@@ -0,0 +1,6 @@
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function GlobalToast(): JSX.Element {
+  return <ToastContainer />;
+}
