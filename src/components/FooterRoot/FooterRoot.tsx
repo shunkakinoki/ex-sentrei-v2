8e06053c879d0d63034a4804f13c66f2c65ebb7c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-30 10:17:43 +0900

    perf: ⚡️ iini env config

--- src/components/FooterRoot/FooterRoot.tsx
+++ src/components/FooterRoot/FooterRoot.tsx
@@ -3,7 +3,8 @@ export default function FooterRoot(): JSX.Element {
     <div className="pt-10">
       <div className="flex px-3 pt-5 pb-5 text-gray-600 border-t-2 border-gray-100">
         <div className="mt-2">
-          Sentrei Inc.&copy; Copyright 2020. All Rights Reserved.
+          Sentrei Inc.&copy; Copyright 2020. All Rights Reserved. v
+          {process.env.SENTREI_VERSION}
         </div>
       </div>
     </div>
