52f12c3390097bf3b05743902d9c10a6b1f1703b
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-30 10:08:45 +0900

    feat: 🎸 ini firebase config

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ module.exports = {
   trailingSlash: false,
   publicRuntimeConfig: {
     NEXT_PUBLIC_SENTREI_VERSION: require("./package.json").version,
+    NEXT_PUBLIC_VERCEL_GITHUB_COMMIT_REF: process.env.VERCEL_GITHUB_COMMIT_REF,
   },
 };
