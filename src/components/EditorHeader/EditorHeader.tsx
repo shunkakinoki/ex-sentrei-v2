64f95e9a665c3e42ff80b964e5241e7f296cf785
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-31 23:40:42 +0900

    perf: ⚡️ remove unnecessary toolkit

--- src/components/EditorHeader/EditorHeader.tsx
+++ src/components/EditorHeader/EditorHeader.tsx
@@ -61,22 +61,6 @@ export default function EditorHeader({
               namespaceId={namespaceId}
             />
           </div>
-          <a className="inline-flex items-center p-2 text-gray-500 rounded-full md:p-3 hover:bg-pink-50">
-            <svg
-              className="w-6 h-6 text-gray-600"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-              />
-            </svg>
-          </a>
           <EditorHeaderSwitch
             switchValue={switchValue}
             setSwitchValue={setSwitchValue}
