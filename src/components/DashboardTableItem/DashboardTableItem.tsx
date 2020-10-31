1aa7998280db40964716c6ae7c9ab853191a4f6e
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-01 00:44:03 +0900

    perf: ⚡️ ini item link

--- src/components/DashboardTableItem/DashboardTableItem.tsx
+++ src/components/DashboardTableItem/DashboardTableItem.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import Link from "next/link";
 
+import useAuth from "@/hooks/useAuth";
 import Article from "@/types/Article";
 
 export interface Props
@@ -20,6 +21,8 @@ export default function DashboardTableItem({
   status,
   namespaceId,
 }: Props): JSX.Element {
+  const {authState} = useAuth();
+
   return (
     <div className="flex flex-col w-full md:flex md:flex-row md:items-center md:justify-between">
       <div className="flex-1 min-w-0">
@@ -84,11 +87,16 @@ export default function DashboardTableItem({
             </div>
           </a>
         </Link>
-        <span className="ml-3 rounded-md shadow-sm group">
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md group-hover:text-pink-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50"
-          >
+        <a
+          href={`https://${
+            // eslint-disable-next-line no-nested-ternary
+            namespaceId !== "" ? namespaceId : authState?.uid ?? "demo"
+          }.sentrei.com${namespaceId === "demo" ? "/demo" : ""}/${slug}`}
+          className="ml-3 rounded-md shadow-sm group"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md group-hover:text-pink-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50">
             <svg
               className="w-5 h-5 mr-2 -ml-1 group-hover:text-pink-500"
               fill="currentColor"
@@ -102,8 +110,8 @@ export default function DashboardTableItem({
               />
             </svg>
             View
-          </button>
-        </span>
+          </div>
+        </a>
         <span className="ml-3 rounded-md shadow-sm">
           <button
             type="button"
