bde674da797e14cbe3cd2bd67ddf3eb812c10e1c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-10-31 12:53:05 +0900

    perf: ⚡️ refactor auth screen

--- src/components/AuthScreen/AuthScreen.tsx
+++ src/components/AuthScreen/AuthScreen.tsx
@@ -1,15 +1,31 @@
 import dynamic from "next/dynamic";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 import {Props as AuthFormProps} from "@/components/AuthForm";
 import ContainerCenter from "@/components/ContainerCenter";
 import ContainerRoot from "@/components/ContainerRoot";
 import HeaderRoot from "@/components/HeaderRoot";
+import useAuth from "@/hooks/useAuth";
 
 const AuthForm = dynamic(() => import("@/components/AuthForm"), {ssr: false});
 
 export type Props = AuthFormProps;
 
 export default function AuthScreen({type}: Props): JSX.Element {
+  const {isLoggedIn} = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    async function pushDemo() {
+      if (isLoggedIn) {
+        await router.push("/dashboard");
+      }
+    }
+    // eslint-disable-next-line no-void
+    void pushDemo();
+  }, [router, isLoggedIn]);
+
   return (
     <ContainerRoot>
       <HeaderRoot />
