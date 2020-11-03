7255c28103786c9426fc1eab7e559385d78c9490
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-03 23:40:49 +0900

    perf: ⚡️ ini space field for user function

--- functions/src/users/onCreate/usersBatchSet.ts
+++ functions/src/users/onCreate/usersBatchSet.ts
@@ -48,12 +48,14 @@ const userBatchSet = functions.auth.user().onCreate(async user => {
 
   const spaceData: Space.Response = {
     articleCount: 0,
+    customerCount: 0,
     authorUids: [user.uid],
     createdAt: timestamp,
     createdBy: profileData,
     createdByUid: user.uid,
     description: null,
     plan: "free",
+    memberCount: 1,
     title: `${profileData.name}'s Publication`,
     image: user.photoURL || null,
     namespaceId: user.uid,
