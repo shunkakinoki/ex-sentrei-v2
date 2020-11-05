6c8122fb352f5e6e23f06dbb4ada3fa9b1bfb195
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-05 11:15:20 +0900

    feat: 🎸 ini story switch custom layout flex

--- src/components/SpaceStoryCard/SpaceStoryCard.tsx
+++ src/components/SpaceStoryCard/SpaceStoryCard.tsx
@@ -41,13 +41,18 @@ export default function SpaceStoryCard({
         className={clsx(
           "w-full my-4 px-0 sm:px-2 md:px-3 xl:px-4 shadow-lg sm:shadow-none sm:my-2 ",
           {
-            "md:w-1/2  lg:w-1/3": storySwitch && variant === "small",
-            "md:w-1/2  lg:w-2/3": storySwitch && variant === "medium",
-            "md:w-full": !storySwitch,
+            "md:w-1/2 lg:w-1/3": storySwitch && variant === "small",
+            "md:w-1/2 lg:w-2/3": storySwitch && variant === "medium",
+            "md:w-full py-2": !storySwitch,
           },
         )}
       >
-        <div className="rounded sm:rounded-2xl md:rounded-lg group">
+        <div
+          className={clsx("rounded sm:rounded-2xl md:rounded-lg group flex", {
+            "md:flex-col": storySwitch,
+            "md:flex-row items-center": !storySwitch,
+          })}
+        >
           <SpaceStoryImage image={image} title={title} />
           <SpaceStoryHero
             time={time}
