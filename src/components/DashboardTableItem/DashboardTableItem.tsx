03604ab897c9a1bdd9d06dda8541bcb6b550c0eb
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-07 00:42:06 +0900

    perf: ⚡️ refactor table item

--- src/components/DashboardTableItem/DashboardTableItem.tsx
+++ src/components/DashboardTableItem/DashboardTableItem.tsx
@@ -68,12 +68,31 @@ export default function DashboardTableItem({
         </div>
       </div>
       <div className="flex mt-5 md:mt-0 md:ml-4">
+        <a className="rounded-md shadow-sm cursor-not-allowed group">
+          <div className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md group-hover:text-pink-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50">
+            <svg
+              className="w-5 h-5 mr-2 -ml-1 group-hover:text-pink-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+              />
+            </svg>
+            Analytics
+          </div>
+        </a>
         <Link
           href={`${
             namespaceId !== "" ? "/" : ""
           }${namespaceId}/dashboard/editor/${id}`}
         >
-          <a className="rounded-md shadow-sm group">
+          <a className="ml-3 rounded-md shadow-sm group">
             <div className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md group-hover:text-pink-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50">
               <svg
                 className="w-5 h-5 mr-2 -ml-1 group-hover:text-pink-500"
@@ -87,33 +106,17 @@ export default function DashboardTableItem({
             </div>
           </a>
         </Link>
+
         <a
           href={`https://${
-            namespaceId !== "demo" ? space?.namespaceId ?? "" : "demo"
+            namespaceId !== "demo" ? space?.namespaceId ?? "" : "demo."
           }${space?.namespaceId !== undefined ? "." : ""}sentrei.com${
             namespaceId === "demo" ? "/demo" : ""
           }/${id}`}
-          className="ml-3 rounded-md shadow-sm group"
           target="_blank"
           rel="noreferrer"
+          className="ml-3 rounded-md shadow-sm"
         >
-          <div className="inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 bg-white border border-gray-300 rounded-md group-hover:text-pink-500 focus:outline-none focus:shadow-outline-pink focus:border-pink-300 active:text-gray-800 active:bg-gray-50">
-            <svg
-              className="w-5 h-5 mr-2 -ml-1 group-hover:text-pink-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z"
-                clipRule="evenodd"
-              />
-            </svg>
-            View
-          </div>
-        </a>
-        <span className="ml-3 rounded-md shadow-sm">
           <button
             type="button"
             className={clsx(
@@ -155,7 +158,7 @@ export default function DashboardTableItem({
             {status === "preview" && "Preview"}
             {status === "published" && "Published"}
           </button>
-        </span>
+        </a>
       </div>
     </div>
   );
