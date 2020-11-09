8a2580e1fa1a493e2b2dab1d9c4a954711759db6
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-10 00:29:59 +0900

    perf: ⚡️ refactor mission body

--- src/components/LandingMission/LandingMission.tsx
+++ src/components/LandingMission/LandingMission.tsx
@@ -9,9 +9,10 @@ export default function LandingMission(): JSX.Element {
           <h3 className="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
             The publishing tool that the internet truly deserves.
           </h3>
-          <p className="max-w-2xl mt-4 text-xl leading-7 text-gray-500 lg:mx-auto">
-            We just want to be fair to both readers and writers. Built alone in
-            Tokyo.
+          <p className="max-w-3xl mt-4 text-xl leading-7 text-gray-500 lg:mx-auto">
+            We just want to be fair to both readers and writers. We feel that
+            popular publishing tools are evil and being dishonest. We just want
+            to do better. Built alone in Tokyo.
           </p>
         </div>
       </div>
