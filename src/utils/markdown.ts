1f6cbd399b260eebb075effe1c94d6feb8acab6c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-11 02:26:38 +0900

    perf: ⚡️ refactor markdown utils

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,11 +1,9 @@
 import fs from "fs";
 import {join} from "path";
 
-const postsDirectory = join(process.cwd(), "legal");
-
 export default function getPostBySlug(slug: string): string {
   const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
+  const fullPath = join("legal", `${realSlug}.md`);
   const body = fs.readFileSync(fullPath, "utf8");
   return body;
 }
