2ea6613d0ac5e1e5f7994256ff188c7b15b0802c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-15 01:03:17 +0900

    perf: ⚡️ ini article space

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -10,16 +10,19 @@ import ArticleScreen, {
 } from "@/components/ArticleScreen";
 import {getAdminArticle, getAdminArticles} from "@/servicesAdmin/Article";
 import {getAdminProfile} from "@/servicesAdmin/Profile";
+import {getAdminSpace} from "@/servicesAdmin/Space";
 import Article from "@/types/Article";
 import Profile from "@/types/Profile";
+import Space from "@/types/Space";
 
 export type Props = Omit<
   ArticleScreenProps,
-  "authors" | "article" | "moreArticles" | "namespaceId"
+  "authors" | "article" | "moreArticles" | "namespaceId" | "space"
 > & {
   article: string;
   authors: string;
   moreArticles: string;
+  space: string;
 };
 
 // eslint-disable-next-line @typescript-eslint/require-await
@@ -42,18 +45,26 @@ export const getStaticProps: GetStaticProps<Props> = async ({
       };
     }
 
-    const author = await getAdminProfile(article?.authorUids[0]);
-    const articles = await getAdminArticles({
+    const authorReq = getAdminProfile(article?.authorUids[0]);
+    const articlesReq = getAdminArticles({
       limit: 2,
       spaceId: article.spaceId,
       start: article.spaceNum ? article.spaceNum + 1 : undefined,
     });
+    const spaceReq = getAdminSpace(article.spaceId);
+
+    const [author, articles, space] = await Promise.all([
+      authorReq,
+      articlesReq,
+      spaceReq,
+    ]);
 
     return {
       props: {
         article: JSON.stringify(article),
         authors: JSON.stringify([author]),
         moreArticles: JSON.stringify(articles),
+        space: JSON.stringify(space),
       },
       revalidate: 30,
     };
@@ -68,12 +79,14 @@ const Slug = ({
   article,
   authors,
   moreArticles,
+  space,
 }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element => {
   return (
     <ArticleScreen
       article={JSON.parse(article) as Article.Get}
       authors={JSON.parse(authors) as Profile.Get[]}
       moreArticles={JSON.parse(moreArticles) as Article.Get[]}
+      space={JSON.parse(space) as Space.Get}
       namespaceId=""
     />
   );
