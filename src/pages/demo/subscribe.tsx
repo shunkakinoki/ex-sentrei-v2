aba9a30a871ef723a4dc8fabff66e01b935971f2
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-15 06:12:57 +0900

    perf: ⚡️ ini demo subscribe

--- src/pages/demo/subscribe.tsx
+++ src/pages/demo/subscribe.tsx
@@ -1,9 +1,32 @@
-import {NextPage} from "next";
+import {GetStaticProps, InferGetStaticPropsType} from "next";
 
-import SubscribeScreen from "@/components/SubscribeScreen";
+import DemoSubscribeScreen, {
+  Props as DemoSubscribeScreenProps,
+} from "@/components/DemoSubscribeScreen";
+import Space from "@/types/Space";
+import {createSpace} from "@/utils/faker";
 
-const Subscribe: NextPage = () => {
-  return <SubscribeScreen />;
+export type Props = Omit<
+  DemoSubscribeScreenProps,
+  "namespaceId" | "spaceId" | "space"
+> & {
+  space: string;
+};
+// eslint-disable-next-line @typescript-eslint/require-await
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const space = createSpace();
+
+  return {
+    props: {
+      space: JSON.stringify(space),
+    },
+  };
+};
+
+const Subscribe = ({
+  space,
+}: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element => {
+  return <DemoSubscribeScreen space={JSON.parse(space) as Space.Get} />;
 };
 
 export default Subscribe;
