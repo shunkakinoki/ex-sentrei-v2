0b124c9d16a4c34056de42e6c8ee0b3d482aa532
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-14 14:06:44 +0900

    perf: ⚡️ ini padding body

--- src/components/ArticleBody/ArticleBody.tsx
+++ src/components/ArticleBody/ArticleBody.tsx
@@ -19,7 +19,7 @@ export default function ArticleBody({body, excerpt}: Props): JSX.Element {
   return (
     <div
       className={clsx(
-        "max-w-sm mx-auto my-3 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl sm:mt-8 md:mt-12 lg:mt-24 xl:mt-32",
+        "max-w-sm mx-auto my-3 px-1 pb-1 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl sm:mt-8 md:mt-12 lg:mt-24 xl:mt-32",
         excerpt &&
           `overflow-hidden relative block text-gray-900 ${Excerpt.excerpt}`,
       )}
