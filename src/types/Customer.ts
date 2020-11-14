4ede5132ad1b9c8fb96623db7c21568f34b981e5
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-14 20:10:42 +0900

    feat: 🎸 upgrade customer model

--- src/types/Customer.ts
+++ src/types/Customer.ts
@@ -1,7 +1,32 @@
-export default interface Props {
-  date: Date;
-  email: string;
-  image?: string;
-  name: string;
-  status: "active";
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+import Metadata from "@/types/Metadata";
+import Profile from "@/types/Profile";
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Customer {
+  export type EditableFields = {
+    email: string;
+    status: "active";
+  };
+
+  type Fields = EditableFields;
+
+  export type AdminUpdate = Partial<Fields>;
+
+  export interface Response extends Fields, Profile.Get, Metadata.Response {}
+
+  export interface Create extends Fields, Profile.Get, Metadata.Create {}
+
+  export interface Update extends Partial<EditableFields>, Metadata.Update {}
+
+  export interface Get extends Fields, Profile.Get, Metadata.Get {
+    id: string;
+  }
+
+  export interface Snapshot extends Get {
+    snap: firebase.default.firestore.DocumentSnapshot;
+  }
 }
+
+export default Customer;
