70693a75541e6f5eff53df2367a40064bae8df13
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-15 13:46:40 +0900

    perf: ⚡️ ini customer update

--- functions/src/profiles/onUpdate/index.ts
+++ functions/src/profiles/onUpdate/index.ts
@@ -1,2 +1,3 @@
 export {default as activityProfileUpdate} from "./activityProfileUpdate";
+export {default as customerProfileUpdate} from "./customerProfileUpdate";
 export {default as userProfileUpdate} from "./userProfileUpdate";
