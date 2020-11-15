cbb8590eefdb1cb40c2ad0327bf0d9c8284c61f4
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-15 17:05:03 +0900

    perf: ⚡️ remove mb more

--- src/components/ArticleStoryGrid/ArticleStoryGrid.tsx
+++ src/components/ArticleStoryGrid/ArticleStoryGrid.tsx
@@ -13,7 +13,7 @@ export default function ArticleStoryGrid({
 }: Props): JSX.Element {
   return (
     <section className="container w-full mx-auto mt-4 mb-8 sm:mt-8 md:mt-16 lg:mt-24 xl:mt-32 ">
-      <div className="flex items-center justify-center pb-6 md:justify-start md:mb-8 lg:mb-12">
+      <div className="flex items-center justify-center md:justify-start">
         <div className="flex-grow-0 px-4 py-2 m-2 font-semibold text-center text-gray-600">
           Other articles from {updatedBy?.name}
         </div>
