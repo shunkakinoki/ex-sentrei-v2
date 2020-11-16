255471387550fb0467e3e1b7727e8f04c1daaf56
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-17 08:44:20 +0900

    feat: 🎸 refactor sales hero

--- src/components/DashboardSalesHero/DashboardSalesHero.tsx
+++ src/components/DashboardSalesHero/DashboardSalesHero.tsx
@@ -4,53 +4,150 @@ export type Props = Sales;
 
 export default function DashboardSalesHero({
   all,
-  allConfirmed,
   month,
-  monthConfirmed,
   confirmed,
 }: Props): JSX.Element {
   return (
-    <div className="container p-3 m-auto md:p-4 text-grey-darkest">
-      <div className="flex flex-col w-full mb-8 -mx-2 md:flex-row ">
-        <div className="w-full px-4 py-2 m-2 text-center text-gray-700 bg-gray-100 md:w-1/3 rounded-xl">
-          <h2 className="m-3 text-xl font-semibold leading-9 text-left text-gray-900 font-display ">
-            All-time sales
-          </h2>
-          <div className="flex items-center justify-start mt-2 mb-1 font-display">
-            <span className="flex items-center px-3 text-2xl font-medium leading-none tracking-tight text-gray-900 md:text-4xl">
-              <span className="mx-2 text-xl leading-none md:text-2xl">$</span>
-              <span>{all.toLocaleString()}</span>
-            </span>
+    <div className="container max-w-6xl px-4 mx-auto sm:px-6 lg:px-8">
+      <h2 className="text-lg font-medium leading-6 text-gray-900">Overview</h2>
+      <div
+        className="grid grid-cols-1 gap-5 mt-2 sm:grid-cols-2 lg:grid-cols-3"
+        x-max="1"
+      >
+        <div className="overflow-hidden bg-white rounded-lg shadow">
+          <div className="p-5">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <svg
+                  className="w-6 h-6 text-gray-400"
+                  x-description="Heroicon name: scale"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                  />
+                </svg>
+              </div>
+              <div className="flex-1 w-0 ml-5">
+                <dl>
+                  <dt className="text-sm font-medium leading-5 text-gray-500 truncate">
+                    All-time sales
+                  </dt>
+                  <dd>
+                    <div className="text-lg font-medium leading-7 text-gray-900">
+                      ${all.toLocaleString()}
+                    </div>
+                  </dd>
+                </dl>
+              </div>
+            </div>
           </div>
-          <h2 className="m-3 font-medium leading-9 text-left text-gray-700 text-md ">
-            Received: $&nbsp;{allConfirmed.toLocaleString()}
-          </h2>
+          <div className="px-5 py-3 bg-gray-50">
+            <div className="text-sm leading-5">
+              <a
+                href="#"
+                className="font-medium text-teal-600 transition duration-150 ease-in-out hover:text-teal-900"
+              >
+                View all
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="w-full px-4 py-2 m-2 text-center text-gray-700 bg-gray-100 md:w-1/3 rounded-xl">
-          <h2 className="m-3 text-xl font-semibold leading-9 text-left text-gray-900 font-display ">
-            This month&apos;s sales
-          </h2>
-          <div className="flex items-center justify-start mt-2 mb-1 font-display">
-            <span className="flex items-center px-3 text-2xl font-medium leading-none tracking-tight text-gray-900 md:text-4xl">
-              <span className="mx-2 text-xl leading-none md:text-2xl">$</span>
-              <span>{month.toLocaleString()}</span>
-            </span>
+        <div className="overflow-hidden bg-white rounded-lg shadow">
+          <div className="p-5">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <svg
+                  className="w-6 h-6 text-gray-400"
+                  x-description="Heroicon name: refresh"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                  />
+                </svg>
+              </div>
+              <div className="flex-1 w-0 ml-5">
+                <dl>
+                  <dt className="text-sm font-medium leading-5 text-gray-500 truncate">
+                    This month&apos;s sales
+                  </dt>
+                  <dd>
+                    <div className="text-lg font-medium leading-7 text-gray-900">
+                      ${month.toLocaleString()}
+                    </div>
+                  </dd>
+                </dl>
+              </div>
+            </div>
           </div>
-          <h2 className="m-3 font-medium leading-9 text-left text-gray-700 text-md ">
-            Received: $&nbsp;{monthConfirmed.toLocaleString()}
-          </h2>
+          <div className="px-5 py-3 bg-gray-50">
+            <div className="text-sm leading-5">
+              <a
+                href="#"
+                className="font-medium text-teal-600 transition duration-150 ease-in-out hover:text-teal-900"
+              >
+                View all
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="w-full px-4 py-2 m-2 text-center text-gray-700 bg-gray-100 md:w-1/3 rounded-xl">
-          <h2 className="m-3 text-xl font-semibold leading-9 text-left text-gray-900 font-display ">
-            Confirmed sales
-          </h2>
-          <div className="flex items-center justify-start mt-2 mb-1 font-display">
-            <span className="flex items-center px-3 text-2xl font-medium leading-none tracking-tight text-gray-900 md:text-4xl">
-              <span className="mx-2 text-xl leading-none md:text-2xl">$</span>
-              <span>{confirmed.toLocaleString()}</span>
-            </span>
+        <div className="overflow-hidden bg-white rounded-lg shadow">
+          <div className="p-5">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <svg
+                  className="w-6 h-6 text-gray-400"
+                  x-description="Heroicon name: check-circle"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+              <div className="flex-1 w-0 ml-5">
+                <dl>
+                  <dt className="text-sm font-medium leading-5 text-gray-500 truncate">
+                    Confirmed sales
+                  </dt>
+                  <dd>
+                    <div className="text-lg font-medium leading-7 text-gray-900">
+                      ${confirmed.toLocaleString()}
+                    </div>
+                  </dd>
+                </dl>
+              </div>
+            </div>
           </div>
-          <div className="py-3" />
+          <div className="px-5 py-3 bg-gray-50">
+            <div className="text-sm leading-5">
+              <a
+                href="#"
+                className="font-medium text-teal-600 transition duration-150 ease-in-out hover:text-teal-900"
+              >
+                View all
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
