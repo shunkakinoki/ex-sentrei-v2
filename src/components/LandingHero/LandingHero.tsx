f92fd8e5dd47682b515e7c2aba8ce354fba48afc
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-16 20:34:22 +0900

    feat: 🎸 ini hero svg

--- src/components/LandingHero/LandingHero.tsx
+++ src/components/LandingHero/LandingHero.tsx
@@ -4,7 +4,75 @@ import LandingHeroTyped from "@/components/LandingHeroTyped";
 
 export default function LandingHero(): JSX.Element {
   return (
-    <section className="relative pt-3 pb-3 md:pb-6">
+    <section className="relative pt-3 pb-3 overflow-hidden md:pb-6">
+      <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full">
+        <div className="relative h-full max-w-screen-xl mx-auto">
+          <svg
+            className="absolute transform right-full translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
+            width="404"
+            height="784"
+            fill="none"
+            viewBox="0 0 404 784"
+          >
+            <defs>
+              <pattern
+                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="784"
+              fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+            />
+          </svg>
+          <svg
+            className="absolute transform left-full -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+            width="404"
+            height="784"
+            fill="none"
+            viewBox="0 0 404 784"
+          >
+            <defs>
+              <pattern
+                id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x="0"
+                  y="0"
+                  width="4"
+                  height="4"
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width="404"
+              height="784"
+              fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
+            />
+          </svg>
+        </div>
+      </div>
       <div className="px-4 mx-auto mt-10 sm:px-6">
         <div className="sm:text-left md:text-center">
           <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
