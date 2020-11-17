c67c1618ce19aa3b7701b2e973d46ffe890fac30
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-17 20:39:58 +0900

    perf: ⚡️ remove table divider

--- src/components/DashboardCustomersTable/DashboardCustomersTable.tsx
+++ src/components/DashboardCustomersTable/DashboardCustomersTable.tsx
@@ -41,7 +41,7 @@ export default function DashboardCustomersTable({
     <div className="flex flex-col">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-          <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+          <div className="overflow-hidden sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
               {swrCustomers && swrCustomers.length > 0 && (
                 <thead>
