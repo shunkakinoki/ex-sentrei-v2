e506a4a05f56acad1eae9d898d62bb12d698b720
Shun Kakinoki <shunkakinoki@gmail.com>
2020-11-17 20:31:15 +0900

    perf: ⚡️ refactor customer table empty hero icon

--- src/components/DashboardCustomerTableEmpty/DashboardCustomerTableEmpty.tsx
+++ src/components/DashboardCustomerTableEmpty/DashboardCustomerTableEmpty.tsx
@@ -16,7 +16,7 @@ export default function DashboardCustomerTableEmpty(): JSX.Element {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+              d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
             />
           </svg>
         </div>
